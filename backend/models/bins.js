0b6aee3045d2eba93538d050d932727b5a91fdb5
Alex Drover <alexfdrover@gmail.com>
2021-09-24 21:33:27 -0700

    moved server files to backend directory

---
+++ backend/models/bins.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const { nanoid } = require('nanoid');
+
+const { requestSchema } = require('./requests');
+
+const generateId = () => {
+  return nanoid(6).toLowerCase();
+};
+
+// create new schema
+const binSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: generateId,
+  },
+
+  requests: [requestSchema]
+}, { timestamps: true });
+
+binSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.path = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+module.exports = {
+  binSchema,
+  Bin: mongoose.model('Bin', binSchema),
+};
